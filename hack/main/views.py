0d4a051aeb342e140731d22b8a8aeb192cad5c35
Fish4203 <S3947135@student.rmit.edu.au>
2022-11-03 14:17:56 +1100

    basic table 1

--- hack/main/views.py
+++ hack/main/views.py
@@ -6,14 +6,7 @@ from datetime import datetime
 
 
 def index(request):
-    fish = 1
-
-
-    if request.method == "POST":
-
-        return render(request, 'index.html', {'tree': request.POST["gender"], 'branch': request.POST["pdate"],'branch2': request.POST["text"]})
-
-    return render(request, 'index.html', {'tree': fish})
+    return render(request, 'index.html', {'data': RunDetails.objects.all()})
 
 
 
