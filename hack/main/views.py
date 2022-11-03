eb35606d5b15644d737336cf103d9f61380f3c9b
ChristopherJordanLamb <104189383+ChristopherJordanLamb@users.noreply.github.com>
2022-11-03 11:25:23 +1100

    html

--- hack/main/views.py
+++ hack/main/views.py
@@ -7,6 +7,10 @@ def index(request):
     fish = 1
 
 
+    if request.method == "POST":
+        
+        return render(request, 'index.html', {'tree': request.POST["gender"], 'branch': request.POST["pdate"],'branch2': request.POST["text"]})
+
     return render(request, 'index.html', {'tree': fish})
 
 
